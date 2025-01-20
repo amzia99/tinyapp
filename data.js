b1fb30cc15843a27a9a21c526f49e7d7597c8544
Abdullah Zia <amz1cflp99@gmail.com>
2025-01-19 21:44:12 -0500

    added data.jsq

---
+++ data.js
@@ -0,0 +1,35 @@
+// data code
+// bcrypt for hashing user passwords
+const bcrypt = require("bcryptjs");
+
+// URL Database
+const urlDatabase = {
+  b6UTxQ: { longURL: "https://www.tsn.ca", userID: "aJ48lW" },
+  i3BoGr: { longURL: "https://www.google.ca", userID: "aJ48lW" },
+};
+
+// Users Database
+const users = {
+  aJ48lW: {
+    id: "aJ48lW",
+    email: "user@example.com",
+    password: bcrypt.hashSync("password", 10), // prehashed user password
+  },
+};
+
+// Function to filter URLs for a specific user
+const urlsForUser = (id) => {
+  const userURLs = {};
+  for (const urlID in urlDatabase) {
+    if (urlDatabase[urlID].userID === id) {
+      userURLs[urlID] = urlDatabase[urlID];
+    }
+  }
+  return userURLs;
+};
+
+module.exports = {
+  urlDatabase,
+  users,
+  urlsForUser,
+};
